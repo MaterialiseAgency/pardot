9ce52ae81af9a6a9029df21c216e76725f5fb12f
Ganna Varakuta <ganna.varakuta@materialise.kiev.ua>
2022-04-25 09:41:33 +0200

    update script

--- public/form_validation.js
+++ public/form_validation.js
@@ -1,43 +1,17 @@
-let elements = document.querySelectorAll('p.required');
-let inputs = document.querySelectorAll('p.required>input');
-let selects = document.querySelectorAll('p.required>select');
-            
-for(let i=0; i<elements.length; i++){
+const elements = document.querySelectorAll('p.required');
+function Error(element){    
+    for(let i=0; i<elements.length; i++){
         let el = elements[i];
-        let input = el.querySelector('input');
-        if(input){
-        input.onblur = function() {        
-        if (input.value === "") {
-        input.style.border = "1px solid red";
-        el.querySelector('span.error_text').style.display = "block";
-        el.querySelector('label').style.color="red";
-        }
-        };
-
-    input.onfocus = function() {
-        
-        this.style.border = "1px solid #00b1ea";
-        el.querySelector('span.error_text').style.display = "none";
-        el.querySelector('label').style.color="black";
-    };
-}}
-
-for(let i=0; i<elements.length; i++){
-    let el = elements[i];
-    let select = el.querySelector('select');
-    if(select){
-    select.onblur = function() {
-        
-        if (select.value === "") {
-        select.style.border = "1px solid red";
-        el.querySelector('span.error_text').style.display = "block";
-        el.querySelector('label').style.color="red";
-        }
-        };
-    select.onfocus = function() {
-        
-        this.style.border = "1px solid #00b1ea";
-        el.querySelector('span.error_text').style.display = "none";
-        el.querySelector('label').style.color="black";
-    };
-}}
+        if(el.querySelector(element)){
+            el.querySelector(element).onblur = function(){ 
+                if (el.querySelector(element).value === "") {
+                    el.classList.add("custom_error");
+                }
+            };
+        el.querySelector(element).onfocus = function(){                
+                el.classList.remove("custom_error");
+            };
+    }}
+}
+Error('input');
+Error('select');   
