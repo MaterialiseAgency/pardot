0a5b0910d8b80bd74f0e0cbf92af856d7951f8b3
OLEKSANDR MELNYTSKYY <alexander.melnytskyy@materialise.kiev.ua>
2022-07-08 11:06:48 +0200

    39-pardot-41640-add-script-seo-tracking: add scripts

---
+++ public/pages/seo_before_gtm.js
@@ -0,0 +1,5 @@
+// add meta: page url
+let metaLinkUrl = document.createElement("meta");
+metaLinkUrl.setAttribute("property", "og:url");
+metaLinkUrl.content = document.location;
+document.getElementsByTagName("head")[0].appendChild(metaLinkUrl);
