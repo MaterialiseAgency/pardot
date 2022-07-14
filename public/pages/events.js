2c08b7c8b08b926e92c0821dbabfbf1a1fa1a96c
Ganna Varakuta <ganna.varakuta@materialise.kiev.ua>
2022-07-14 16:34:45 +0200

    CTA scripts, icons, fix-style textarea

--- public/pages/events.js
+++ public/pages/events.js
@@ -1,5 +1,35 @@
 // modal functionality
 
-toogleModal = (event) => {
-  document.getElementById("modal").classList.toggle("modal");
+// toogleModal = (event) => {  
+//   document.getElementById("modal").classList.toggle("modal");
+// };
+let book_url = document.getElementById("book-link").href;
+        let book_link = document.getElementById("book-link");
+        if(book_url !== location.href){
+             document.getElementById("modal").style.display = "none";
+          }else{
+          document.addEventListener('click', function (e) {
+            if (e.target.id === book_link.id) {
+                e.preventDefault();
+            }
+        });
+          toogleModal = (event) => {  
+            document.getElementById("modal").classList.toggle("modal");
+          }
+        };
+
+let url = document.getElementById("register-link").href;
+let link = document.getElementById("register-link");
+        
+if(url !== location.href){
+  document.getElementById("modal").style.display = "none";
+}else{
+  document.addEventListener('click', function (e) {
+        if (e.target.id === link.id) {
+            e.preventDefault();
+        }
+    });
+  toogleModal = (event) => {  
+    document.getElementById("modal").classList.toggle("modal");
+  }
 };
