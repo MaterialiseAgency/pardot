6a789eb9f5f6d7b810a47ecd8ca9706557949a75
Ganna Varakuta <ganna.varakuta@materialise.kiev.ua>
2022-07-15 16:13:51 +0200

    update

--- public/pages/events.js
+++ public/pages/events.js
@@ -3,33 +3,21 @@
 // toogleModal = (event) => {  
 //   document.getElementById("modal").classList.toggle("modal");
 // };
-let book_url = document.getElementById("book-link").href;
-        let book_link = document.getElementById("book-link");
-        if(book_url !== location.href){
-             document.getElementById("modal").style.display = "none";
-          }else{
-          document.addEventListener('click', function (e) {
-            if (e.target.id === book_link.id) {
+
+const link = document.getElementsByClassName("register-link");
+Array.from(link).forEach(function(elem) {  
+  elem.onclick = function(){    
+    if(elem.href !== location.href){
+      document.getElementById("modal").style.display = "none";
+    }else{
+      document.addEventListener('click', function (e) {
+            if (e.target.id === elem.id) {
                 e.preventDefault();
             }
         });
-          toogleModal = (event) => {  
-            document.getElementById("modal").classList.toggle("modal");
-          }
-        };
-
-let url = document.getElementById("register-link").href;
-let link = document.getElementById("register-link");
-        
-if(url !== location.href){
-  document.getElementById("modal").style.display = "none";
-}else{
-  document.addEventListener('click', function (e) {
-        if (e.target.id === link.id) {
-            e.preventDefault();
-        }
-    });
-  toogleModal = (event) => {  
-    document.getElementById("modal").classList.toggle("modal");
-  }
-};
+      toogleModal = (event) => {  
+        document.getElementById("modal").classList.toggle("modal");
+      }
+    };
+  };
+});
