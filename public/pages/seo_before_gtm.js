9e8a91196cad6771c7120fdeebce6406a2dcff5c
OLEKSANDR MELNYTSKYY <alexander.melnytskyy@materialise.kiev.ua>
2022-08-04 17:31:46 +0200

    Fix scripts, add links

--- public/pages/seo_before_gtm.js
+++ public/pages/seo_before_gtm.js
@@ -1,19 +1,26 @@
 // add meta: page url
-let metaLinkUrl = document.createElement("meta");
+const metaLinkUrl = document.createElement("meta");
 metaLinkUrl.setAttribute("property", "og:url");
 metaLinkUrl.content = document.location;
 document.getElementsByTagName("head")[0].appendChild(metaLinkUrl);
 
-// pushh the channel
-let metaDescription = document.querySelector('meta[name="description"]');
-let metaDescriptionContent = metaDescription.getAttribute("content");
-const regex = /{(.*?)}/gm;
-let foundChannel = metaDescriptionContent.match(regex);
-metaDescriptionContent = metaDescriptionContent.replace(foundChannel, "");
-metaDescription.setAttribute("content", metaDescriptionContent.trim());
-let channel = foundChannel[0].slice(1);
-channel = channel.slice(0, -1);
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-  page: { ownerChannel: channel },
-});
+// push the channel
+const metaDescription = document.querySelector('meta[name="description"]');
+if (metaDescription) {
+  let metaDescriptionContent = metaDescription.getAttribute("content");
+  const regex = /{(.*?)}/gm;
+  let foundChannel = metaDescriptionContent.match(regex);
+  console.log(foundChannel);
+
+  if (foundChannel) {
+    metaDescriptionContent = metaDescriptionContent.replace(foundChannel, "");
+    metaDescription.setAttribute("content", metaDescriptionContent.trim());
+    let channel = foundChannel[0].slice(1);
+    channel = channel.slice(0, -1);
+    console.log(channel);
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      page: { ownerChannel: channel },
+    });
+  }
+}
