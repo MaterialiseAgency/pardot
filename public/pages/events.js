bfe80eba73affe80e2c57b626481d77ac8209827
OLEKSANDR MELNYTSKYY <alexander.melnytskyy@materialise.kiev.ua>
2022-08-05 11:23:36 +0200

    Script setup

--- public/pages/events.js
+++ public/pages/events.js
@@ -1,20 +1,34 @@
 // modal functionality
 
-const link = document.getElementsByClassName("register-link");
+if (document.getElementsByClassName("register-link")) {
+  const link = document.getElementsByClassName("register-link");
 
-Array.from(link).forEach(function(elem) {  
-  elem.onclick = function(){    
-    if(elem.href !== location.href){
-      document.getElementById("modal").style.display = "none";
-    }else{
-      document.addEventListener('click', function (e) {
-            if (e.target.id === elem.id) {
-                e.preventDefault();
-            }
+  Array.from(link).forEach(function (elem) {
+    elem.onclick = function () {
+      if (elem.href !== location.href) {
+        document.getElementById("modal").style.display = "none";
+      } else {
+        document.addEventListener("click", function (e) {
+          if (e.target.id === elem.id) {
+            e.preventDefault();
+          }
         });
-      toogleModal = (event) => {  
-        document.getElementById("modal").classList.toggle("modal");
+        toogleModal = (event) => {
+          document.getElementById("modal").classList.toggle("modal");
+        };
       }
     };
-  };
-});
+  });
+}
+
+//redirect to the event page if no utm on register page
+if (
+  window.location.href.includes("register") &&
+  !window.location.href.includes("crm_campaign")
+) {
+  let registerPageUrl = window.location.href;
+  const getIndex = registerPageUrl.indexOf("register");
+  const registerPageUTMSlice = registerPageUrl.slice(getIndex - 1);
+  const eventPageURL = registerPageUrl.replace(registerPageUTMSlice, "");
+  window.location.replace(eventPageURL);
+}
