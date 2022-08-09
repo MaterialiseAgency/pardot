a54d8a0a1a42a9fc40a6c3e0da29a5038215b2e1
OLEKSANDR MELNYTSKYY <alexander.melnytskyy@materialise.kiev.ua>
2022-08-09 10:31:37 +0200

    Remove console logs

--- public/pages/seo_before_gtm.js
+++ public/pages/seo_before_gtm.js
@@ -7,28 +7,24 @@ document.getElementsByTagName("head")[0].appendChild(metaLinkUrl);
 // push the channel
 if (window.location.href.indexOf("/medical/") > -1) {
   window.dataLayer = window.dataLayer || [];
-  console.log("Medical");
   window.dataLayer.push({
     page: { ownerChannel: "Medical" },
   });
 }
 if (window.location.href.indexOf("/software/")) {
   window.dataLayer = window.dataLayer || [];
-  console.log("Software");
   window.dataLayer.push({
     page: { ownerChannel: "Software" },
   });
 }
 if (window.location.href.indexOf("/manufacturing/")) {
   window.dataLayer = window.dataLayer || [];
-  console.log("Manufacturing");
   window.dataLayer.push({
     page: { ownerChannel: "Manufacturing" },
   });
 }
 if (window.location.href.indexOf("/corporate/")) {
   window.dataLayer = window.dataLayer || [];
-  console.log("Corporate");
   window.dataLayer.push({
     page: { ownerChannel: "Corporate" },
   });
