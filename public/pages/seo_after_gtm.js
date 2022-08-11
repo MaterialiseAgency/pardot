ba4680994fe5f9b0434bf034d2ebc735aa217dc3
OLEKSANDR MELNYTSKYY <alexander.melnytskyy@materialise.kiev.ua>
2022-08-11 15:46:23 +0200

    SEO Updates

--- public/pages/seo_after_gtm.js
+++ public/pages/seo_after_gtm.js
@@ -1,12 +1,17 @@
 // add meta: page OG image
 const metaLinkOgImage = document.createElement("meta");
 metaLinkOgImage.setAttribute("property", "og:image");
+metaLinkOgImage.setAttribute("name", "image");
 if (document.querySelector("#main_image img")) {
   const mainImageLink = document
     .querySelector("#main_image img")
     .getAttribute("src");
   if (mainImageLink) {
     metaLinkOgImage.content = mainImageLink;
-    document.getElementsByTagName("head")[0].appendChild(metaLinkOgImage);
+    let metaDescription = document.querySelector("[name='description']");
+    function insertAfter(newNode, existingNode) {
+      existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+    }
+    insertAfter(metaLinkOgImage, metaDescription);
   }
 }
