2aedf8aff3193b99375a47f4b585415775376819
OLEKSANDR MELNYTSKYY <alexander.melnytskyy@materialise.kiev.ua>
2022-09-07 11:21:52 +0200

    Add url to the form

--- public/forms/forms.js
+++ public/forms/forms.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("pardot-form").elements[0].focus();
   }
 });
+
+//get url
+const url = window.location.href;
+if (document.querySelector(".crm_customer_url input")) {
+  const input = document.querySelector(".crm_customer_url input");
+  input.value = url;
+}
